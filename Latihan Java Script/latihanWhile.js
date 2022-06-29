bfd6479e27b6cb42355daa7a842ac98c909144fa
riwantositinjak <riwanto1212@gmail.com>
2022-06-29 15:59:01 +0700

    menambahkan latihan penggunaan while pada Javascript

---
+++ Latihan Java Script/latihanWhile.js
@@ -0,0 +1,23 @@
+// membuat sebuah file javascript dengan menampilkan sebuah pesan
+// Angkot No.1 beroperasi dengan baik. sebanyak 10 kali di console
+
+var i = 1; 
+var j = 10;
+
+while (i <= j) {
+    console.log('Angkot No. ' + i + ' beroperasi dengan baik');
+    i++;
+}
+
+//output dari code diatas jika di running 
+
+// mencetak string sebanyak 1kali
+// mencetak string sebanyak 2kali
+// mencetak string sebanyak 3kali
+// mencetak string sebanyak 4kali
+// mencetak string sebanyak 5kali
+// mencetak string sebanyak 6kali
+// mencetak string sebanyak 7kali
+// mencetak string sebanyak 8kali
+// mencetak string sebanyak 9kali
+// mencetak string sebanyak 10kali
