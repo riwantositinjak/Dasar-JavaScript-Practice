0c95d35af86003aa199123eb2316f13a493a1116
riwantositinjak <riwanto1212@gmail.com>
2022-06-29 17:54:13 +0700

    menambah file latihan JavaScript

--- Latihan Java Script/pengulanganFor3.js
+++ Latihan Java Script/pengulanganFor3.js
@@ -1,16 +1,27 @@
+// Deklarasi Variable 
 var jmlAngkot = 10; 
 var angkotBeroperasi = 6;
 var noAngkot = 1;
 
+// looping for
 for (noAngkot; noAngkot <= jmlAngkot; noAngkot++){
+    
+    // pengkondisian if
     if(noAngkot <= angkotBeroperasi){ 
     console.log('Angkot No. ' + noAngkot + ' beroperasi dengan baik');
-    }else {
+    }
+    //pengkondisian else if
+    else if(noAngkot === 8 || noAngkot === 10) { // pengkondisian jika noAngkot 8 dan 10 maka akan di eksekusi di console.
+        console.log('Angkot No. ' + noAngkot + ' sedang lembur');
+    }
+    //pengkondisian else 
+    else {
         console.log('Angkot No. ' + noAngkot+ ' sedang tidak beroperasi')  ; 
-    } 
+    }
 }
 
 // output dari code diatas 
+
 // Angkot No. 1 beroperasi dengan baik
 // Angkot No. 2 beroperasi dengan baik
 // Angkot No. 3 beroperasi dengan baik
@@ -18,6 +29,6 @@ for (noAngkot; noAngkot <= jmlAngkot; noAngkot++){
 // Angkot No. 5 beroperasi dengan baik
 // Angkot No. 6 beroperasi dengan baik
 // Angkot No. 7 sedang tidak beroperasi
-// Angkot No. 8 sedang tidak beroperasi
+// Angkot No. 8 sedang lembur
 // Angkot No. 9 sedang tidak beroperasi
-// Angkot No. 10 sedang tidak beroperasi
+// Angkot No. 10 sedang lembur
