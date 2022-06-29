d9d0eefe7115d20aa544b413bb833df383b86ef7
riwantositinjak <riwanto1212@gmail.com>
2022-06-29 15:45:40 +0700

    menambah file javascript dan mengubah nama file javascript

---
+++ Dasar JavaScript/pengulanganWhile.js
@@ -0,0 +1,24 @@
+// menampilkan hello world sebanyak 10 kali di console menggunakan
+// pengulangan while 
+
+var a = 1; 
+var b = 10; // ini variabel untuk mengecek kondisi pengulangan pada while 
+
+
+while( a <= b) {
+    console.log('Hello World') // mencetak sebuah string 'Hello World' di console
+    a++; // ini proses aksi yang menyatakan variabel a akan di increment sesuai kondisi a <= b
+}
+
+// output dari code diatas 
+
+// Hello World
+// Hello World
+// Hello World
+// Hello World
+// Hello World
+// Hello World
+// Hello World
+// Hello World
+// Hello World
+// Hello World
---
+++ Dasar JavaScript/pengulanganWhile2.js
@@ -0,0 +1,21 @@
+// mencetak string dengan perulangan while di console.log
+
+var s = 1;
+var m = 10;
+
+while( s <= m ){
+    console.log("mencetak string sebanyak " + s + "kali");
+    s++;
+}
+// output dari code ini 
+
+// mencetak string sebanyak 1kali
+// mencetak string sebanyak 2kali
+// mencetak string sebanyak 3kali
+// mencetak string sebanyak 4kali
+// mencetak string sebanyak 5kali
+// mencetak string sebanyak 6kali
+// mencetak string sebanyak 7kali
+// mencetak string sebanyak 8kali
+// mencetak string sebanyak 9kali
+// mencetak string sebanyak 10kali
