9a8bf2fb34d851e4d90767f5e19e6d04a181afe4
riwantositinjak <riwanto1212@gmail.com>
2022-09-12 03:17:33 +0700

    update

--- freeCodeCampBasic/50. return-a-value-from-a-function-with-return.js
+++ freeCodeCampBasic/50. return-a-value-from-a-function-with-return.js
@@ -1,4 +1,21 @@
-function timesFive(args){
+/* Return a Value from a Function with Return
+We can pass values into a function with arguments. You can use a return statement to send a value back out of a function.
+
+Example
+
+function plusThree(num) {
+  return num + 3;
+}
+
+const answer = plusThree(5);
+answer has the value 8.
+
+plusThree takes an argument for num and returns a value equal to num + 3.
+
+Create a function timesFive that accepts one argument, multiplies it by 5, and returns the new value. */
+
+// problem solving
+function timesFive(args) {
   return args * 5;
 }
 timesFive(5);
