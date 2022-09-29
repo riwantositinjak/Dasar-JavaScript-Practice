54dbb5e1f11be6d3abf8beadd03b12f7a718282b
riwantositinjak <riwanto1212@gmail.com>
2022-09-29 13:31:40 +0700

    adding aVeryBigIntSum challenge HackerRank

---
+++ HackerRank Problem Solving/aVeryBigSumInt.js
@@ -0,0 +1,44 @@
+// In this challenge, you are required to calculate and print the sum of the elements in an array, keeping in mind that some of those integers may be quite large.
+
+// Function Description
+
+// Complete the aVeryBigSum function in the editor below. It must return the sum of all array elements.
+
+// aVeryBigSum has the following parameter(s):
+
+// int ar[n]: an array of integers .
+// Return
+
+// long: the sum of all array elements
+// Input Format
+
+// The first line of the input consists of an integer n.
+// The next line contains n space-separated integers contained in the array.
+
+// Output Format
+
+// Return the integer sum of the elements in the array.
+
+// Constraints
+// 1 <= n <= 10
+// 0 <= ar[i]< 10^10
+
+// Sample Input
+// 5
+// 1000000001 1000000002 1000000003 1000000004 1000000005
+// Output
+
+// 5000000015
+
+// Problem Solving
+const ar = [5, 1000000001, 1000000002, 1000000003, 1000000004, 1000000005];
+
+function aVeryBigSumInt(ar) {
+  let sum = 0;
+  for (let i = 0; i < ar.length; i++) {
+    sum += ar[i];
+  }
+  return sum;
+}
+
+console.log(aVeryBigSumInt(ar));
