034b9bc8fb25aae532bd7bad8350a8e210a4ea01
riwantositinjak <riwanto1212@gmail.com>
2022-09-30 13:07:26 +0700

    test

---
+++ Latihan Java Script/grade.js
@@ -0,0 +1,14 @@
+// function grade(n) {
+//   switch (n) {
+//     case 80:
+//       return "kamu lulus";
+//     case 70:
+//       return "kamu masih bisa lulus";
+//     default:
+//       return "kamu gagal";
+//   }
+// }
+
+// console.log(grade(80));
+
+
---
+++ Latihan Java Script/summing.js
@@ -0,0 +1,14 @@
+// function sum(ar) {
+//   let sum = 0;
+//   for (let i = 0; i < ar.length; i++) {
+//     sum += ar[i];
+//   }
+//   return sum;
+// }
+
+// console.log(sum([1, 2, 3, 4, 5, 6]));
+
+const arr = [1, 5, 6, 7, 8, 89, 9, 9];
+
+const check = arr.sort();
+console.log(check);
